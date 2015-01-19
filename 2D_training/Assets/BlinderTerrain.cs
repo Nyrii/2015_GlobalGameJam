f2d68754c28532375b0204ff4b89d1556149d84d
Fernand <fernand.veyrier@epitech.eu>
2015-01-19 23:26:20 +0100

    Did super minimap

---
+++ 2D_training/Assets/BlinderTerrain.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinderTerrain : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		Destroy(this.gameObject);
+	}
+	
+}
---
+++ 2D_training/Assets/SpawnBlind.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBlind : MonoBehaviour {
+	
+	public GameObject blindObj;
+	public int startXPos = 0;
+	public int startYPos = 0;
+	public int endXPos = 0;
+	public int endYPos = 0;
+	
+	void Start()
+	{
+		for(int j = startXPos; j <= endXPos; j++)
+		{
+			for (int i = startYPos; i <= endYPos; i++)
+			{
+				GameObject instance = (GameObject) Instantiate(blindObj, new Vector3(i, j, -11), Quaternion.identity);
+				//Makes game veeeeery laggy
+				//instance.transform.parent = this.gameObject.transform;
+			}
+		}
+	}
+}
