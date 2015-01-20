1cd25c27d67f440fcc709e726065f85f1d832ed2
Fernand <fernand.veyrier@epitech.eu>
2015-01-20 14:16:41 +0100

    Bouncing plateform
    bouncy booty

---
+++ 2D_training/Assets/Bouncer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouncer : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		Debug.Log("COLLISION");
+		if (col.gameObject.tag == "Player")
+		{
+			float verticalVel =0;//= col.gameObject.rigidbody2D.velocity.y;
+			verticalVel += 10f;
+			col.gameObject.rigidbody2D.velocity = new Vector2(col.gameObject.rigidbody2D.velocity.x, verticalVel);
+		}
+	}
+}
