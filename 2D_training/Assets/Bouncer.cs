f20b2ce118b8bb3c86cbbdddd891403b7ba258e1
Fernand <fernand.veyrier@epitech.eu>
2015-01-20 18:05:56 +0100

    bouncing

--- 2D_training/Assets/Bouncer.cs
+++ 2D_training/Assets/Bouncer.cs
@@ -3,14 +3,51 @@ using System.Collections;
 
 public class Bouncer : MonoBehaviour {
 
+	public class Stuff
+	{
+		public int bullets;
+		public int grenades;
+		public int rockets;
+		public float fuel;
+		
+		public Stuff(int bul, int gre, int roc)
+		{
+			bullets = bul;
+			grenades = gre;
+			rockets = roc;
+		}
+		
+		public Stuff(int bul, float fu)
+		{
+			bullets = bul;
+			fuel = fu;
+		}
+		
+		// Constructor
+		public Stuff ()
+		{
+			bullets = 1;
+			grenades = 1;
+			rockets = 1;
+		}
+	}
+
+	float verticalVel;
 	void OnCollisionEnter2D (Collision2D col)
 	{
-		Debug.Log("COLLISION");
+		Debug.Log("Collided at "+ col.contacts[0].point);
 		if (col.gameObject.tag == "Player")
 		{
-			float verticalVel =0;//= col.gameObject.rigidbody2D.velocity.y;
-			verticalVel += 10f;
-			col.gameObject.rigidbody2D.velocity = new Vector2(col.gameObject.rigidbody2D.velocity.x, verticalVel);
+			verticalVel = 600f;
+			col.gameObject.rigidbody2D.AddForce(new Vector2(0 , verticalVel));
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D col)
+	{
+		if (col.gameObject.rigidbody2D.velocity != new Vector2(col.gameObject.rigidbody2D.velocity.x, verticalVel))
+		{
+			//col.gameObject.rigidbody2D.velocity = new Vector2(col.gameObject.rigidbody2D.velocity.x, verticalVel);
 		}
 	}
 }
