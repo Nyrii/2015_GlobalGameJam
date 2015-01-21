08da44cc8b816d945d8f4bec4416ea1d44bf5f2d
Fernand <fernand.veyrier@epitech.eu>
2015-01-21 11:45:57 +0100

    Batmaaaaaan
    Ajout d'un batgrapin

---
+++ 2D_training/Assets/BatScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(DistanceJoint2D))]
+public class BatScript : MonoBehaviour {
+
+	//Batlasso
+	LineRenderer lineRend;
+	DistanceJoint2D joint;
+
+	//avoid colliding with blinder
+	public LayerMask WhatToHit;
+
+	void Start () 
+	{
+		lineRend = GetComponent<LineRenderer>();
+		joint = GetComponent<DistanceJoint2D>();
+		joint.enabled = false;
+	}
+	
+	void Update () 
+	{
+		RaycastHit2D ray;
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		ray = Physics2D.Raycast(transform.position, new Vector2(0, 1), 10f, WhatToHit);
+		if (ray.collider != null && Input.GetKey(KeyCode.UpArrow))
+		{
+			joint.enabled = true;
+			lineRend.enabled = true;
+			joint.connectedAnchor = ray.collider.gameObject.transform.position;
+			joint.anchor = new Vector2(0, 0);
+			lineRend.SetPosition(0, transform.position);
+			lineRend.SetPosition(1, ray.collider.gameObject.transform.position);
+			Debug.DrawRay(transform.position, ray.collider.gameObject.transform.position - transform.position);
+		}
+		else
+		{
+			lineRend.enabled = false;
+			joint.enabled = false;
+		}
+	}
+}
