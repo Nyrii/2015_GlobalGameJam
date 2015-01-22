c429fea7aa3098f83fe9538f197f85b2fdf7741f
Fernand <fernand.veyrier@epitech.eu>
2015-01-22 18:01:07 +0100

    no escape
    impossible to fall of the sides

---
+++ 2D_training/Assets/OutOfTerrainBounds.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutOfTerrainBounds : MonoBehaviour {
+
+	public GameObject	OtherLimit;
+
+	GameObject				player;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (player != null && OtherLimit != null)
+		{
+			player.rigidbody2D.velocity = Vector2.zero;
+			player.transform.position = OtherLimit.transform.position + Vector3.up * 3f;
+			player.GetComponent<UnitySampleAssets._2D.Platformer2DUserControl>().enabled = false;
+			player.rigidbody2D.velocity = OtherLimit.transform.up * 15f;
+			Invoke("ResumeControl", 1f);
+		}
+	}
+
+	void ResumeControl()
+	{
+		player.GetComponent<UnitySampleAssets._2D.Platformer2DUserControl>().enabled = true;
+	}
+}
