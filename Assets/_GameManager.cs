ade756b2ddff89e10999138c35a6e53c42b591d7
Fernand <fernand.veyrier@epitech.eu>
2015-01-23 21:13:17 +0100

    adding menus

--- Assets/_GameManager.cs
+++ Assets/_GameManager.cs
@@ -4,18 +4,22 @@ using UnitySampleAssets._2D;
 
 public class _GameManager : MonoBehaviour {
 
+    public int isPlaying;
     //Player components
     GameObject player;
     PlatformerCharacter2D playerControl;
 
     //menu components for pause or quit
     public GameObject canvasObj;
+    public GameObject resumeText;
 
     void Start()
     {
+        isPlaying = 1;
         player = GameObject.FindGameObjectWithTag("Player");
         playerControl = player.GetComponent<PlatformerCharacter2D>();
         canvasObj.SetActive(false);
+        resumeText.SetActive(false);
     }
 
     void Update()
@@ -26,23 +30,43 @@ public class _GameManager : MonoBehaviour {
             canvasObj.SetActive(true);
             Time.timeScale = 0;
         }
+        if (Input.GetKeyDown(KeyCode.Tab) && playerControl.health > 0)
+        {
+            isPlaying = (isPlaying == 0) ? 1 : 0;
+            if (isPlaying == 0)
+            {
+                canvasObj.SetActive(true);
+                resumeText.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                canvasObj.SetActive(false);
+                resumeText.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
     }
 
-    public void pauseMenu(int choice)
+    public void pauseMenuOnDeath(int choice)
     {
         //1 = reload
         //0 = menu
         //-1 = quit
+        isPlaying = 0;
         if (choice == 1)
         {
+            Time.timeScale = 1;
             Application.LoadLevel(Application.loadedLevel);
         }
         else if (choice == 0)
         {
+            Time.timeScale = 1;
             Application.LoadLevel("MainMenu");
         }
         else if (choice == -1)
         {
+            Time.timeScale = 1;
             Application.Quit();
         }
     }
