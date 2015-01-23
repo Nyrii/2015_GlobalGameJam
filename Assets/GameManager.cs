99fc0c1f2ffc9b0ce8db38b16540c3d5d1916e91
Fernand <fernand.veyrier@epitech.eu>
2015-01-23 18:56:23 +0100

    Adding few elements

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public void LoadLevel(int level)
+    {
+        if (level == -1)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            Application.LoadLevel(level);
+        }
+    }
+}
