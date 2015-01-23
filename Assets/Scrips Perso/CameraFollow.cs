61fa8246e51e925802e3b6ec0b1965444591509d
Nyrii <Nyriikx@gmail.com>
2015-01-23 22:27:22 +0100

    TrapWallAnim à faire
    Lis le titre !

---
+++ Assets/Scrips Perso/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform mytarget;
+
+	// Update is called once per frame
+	void Update () {
+		if (mytarget != null) {
+			Vector3 targpos = mytarget.position;
+			targpos.z = transform.position.z;
+			transform.position = targpos;
+				}
+	}
+}
