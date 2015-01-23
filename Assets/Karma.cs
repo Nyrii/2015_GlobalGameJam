12a8af605b0e34d823771d930ec4c80e76b8175c
Fernand <fernand.veyrier@epitech.eu>
2015-01-23 21:53:25 +0100

    Menu

---
+++ Assets/Karma.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Karma : MonoBehaviour {
+
+    public float karmaAmount = 0;
+    float DecreaseVel;
+    public Image karmaBar;
+    _GameManager gm;
+
+    void Start()
+    {
+        gm = GameObject.Find("_GameManager").GetComponent<_GameManager>();
+    }
+
+    void Update()
+    {
+        karmaBar.fillAmount = Mathf.SmoothDamp(karmaBar.fillAmount, karmaAmount, ref DecreaseVel, 0.3f);	
+    }
+}
