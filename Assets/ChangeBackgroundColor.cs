2e49209ec05245728516c0c9c45aaeaf314277e4
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 18:53:33 +0100

    Added background script

---
+++ Assets/ChangeBackgroundColor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySampleAssets._2D;
+
+public class ChangeBackgroundColor : MonoBehaviour {
+
+    PlatformerCharacter2D playerData;
+    public SpriteRenderer blueBG;
+    public SpriteRenderer redBG;
+    float karmaAmount;
+
+    void Start()
+    {
+        playerData = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerCharacter2D>();
+    }
+
+    void Update()
+    {
+        karmaAmount = playerData.karmaAmount;
+        redBG.color = new Color(1, 1, 1, Mathf.Clamp01(1 - 2 * karmaAmount));
+        blueBG.color = new Color(1, 1, 1, Mathf.Clamp01(2 * karmaAmount - 1f));
+    }
+}
