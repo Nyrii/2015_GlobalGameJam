527c20f20f9cd9e2193201b565e6a07fc01555ca
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 02:52:09 +0100

    Adding  gauge

--- Assets/Scrips Perso/Karma.cs
+++ Assets/Scrips Perso/Karma.cs
@@ -4,19 +4,20 @@ using UnityEngine.UI;
 
 public class Karma : MonoBehaviour {
 
-    public static float karmaAmount = 0;
+    public static float karmaAmount = 0.5f;
     float DecreaseVel;
-    public Image karmaBar;
-    public Image karmaBarExtern;
+    private Image karmaBar;
+    private Image karmaBarExtern;
     _GameManager gm;
 
     void Start()
     {
         gm = GameObject.Find("_GameManager").GetComponent<_GameManager>();
+        karmaBar = GetComponent<Image>();
     }
 
     void Update()
     {
-        //karmaBar.fillAmount = Mathf.SmoothDamp(karmaBar.fillAmount, karmaAmount, ref DecreaseVel, 0.3f);	
+        karmaBar.fillAmount = Mathf.SmoothDamp(karmaBar.fillAmount, Mathf.Clamp01(karmaAmount), ref DecreaseVel, 0.3f);	
     }
 }
