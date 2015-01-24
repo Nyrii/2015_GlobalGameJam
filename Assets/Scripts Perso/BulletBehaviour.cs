c4ca912f0ed1e91b987100296bd625821aaaef67
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 23:47:13 +0100

    ??? Commits
    LOLOLOLOL

---
+++ Assets/Scripts Perso/BulletBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    void Update()
+    {
+        if (Camera.main.WorldToScreenPoint(this.transform.position).x > Screen.width || Camera.main.WorldToScreenPoint(this.transform.position).x < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+	void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(this.gameObject);
+    }
+}
