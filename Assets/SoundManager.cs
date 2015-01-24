857b7215b3c7759e85a69619f1899ba5b853fcf9
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 13:34:01 +0100

    adding audio

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    float karma;
+    public GameObject KarmaGauge;
+    public AudioClip[] musics;
+
+    void Start () 
+    {
+        audio.clip = musics[0];
+        audio.Play();
+        audio.loop = true;
+        karma = Karma.karmaAmount;
+	}
+	
+	void Update () 
+    {
+	    if (karma <= 0.6 && karma >= 0.4)
+        {
+            audio.clip = musics[0];
+        }
+        //add amounts
+	}
+}
