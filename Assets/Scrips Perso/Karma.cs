f20fd64e84f8821799de0fa0bc59a51a1b592652
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 04:19:39 +0100

    Gauge OK

--- Assets/Scrips Perso/Karma.cs
+++ Assets/Scrips Perso/Karma.cs
@@ -7,6 +7,7 @@ public class Karma : MonoBehaviour {
 
     public static float karmaAmount = 0.5f;
     float DecreaseVel;
+    float DecreaseVel2;
     private Image karmaEvilGauge;
     private Image karmaGoodGauge;
     _GameManager gm;
@@ -26,7 +27,9 @@ public class Karma : MonoBehaviour {
     {
         karmaAmount = playerStats.karmaAmount;
         Debug.Log(karmaAmount);
-        karmaEvilGauge.fillAmount = Mathf.SmoothDamp(karmaEvilGauge.fillAmount, Mathf.Clamp01(karmaAmount), ref DecreaseVel, 0.3f);
-        karmaGoodGauge.fillAmount = Mathf.SmoothDamp(karmaGoodGauge.fillAmount, Mathf.Clamp01(karmaAmount), ref DecreaseVel, 0.3f);	
+        Debug.Log("load = " + Mathf.Clamp01(1 - 2 * karmaAmount));
+        karmaEvilGauge.fillAmount = Mathf.SmoothDamp(karmaEvilGauge.fillAmount, Mathf.Clamp01(1 - 2 * karmaAmount), ref DecreaseVel, 0.3f);
+        karmaGoodGauge.fillAmount = Mathf.SmoothDamp(karmaGoodGauge.fillAmount, Mathf.Clamp01(2 * karmaAmount - 1f), ref DecreaseVel2, 0.3f);
+        //DecreaseVel = 0.1f;
     }
 }
