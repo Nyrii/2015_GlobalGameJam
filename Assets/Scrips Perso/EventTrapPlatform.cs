43c256a99e7b83d66db5d8becc4825dc2a6241ea
Nyrii <Nyriikx@gmail.com>
2015-01-24 03:49:24 +0100

    Deux plateformes à faire (Evil / Good)

---
+++ Assets/Scrips Perso/EventTrapPlatform.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTrapPlatform : MonoBehaviour {
+	
+	public GameObject Platform02;
+	public GameObject Player;
+	static float Indicate = 0;
+	GameObject Platform01;
+
+	void Start()
+	{
+		Platform01 = gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = Player.transform.position;
+		if ((pos.y > Platform01.transform.position.y) && (pos.x > Platform01.transform.position.x)
+		    && (pos.x < Platform02.transform.position.x + (Platform02.transform.localScale.x / 2)) && Indicate == 0)
+		{
+			Platform01.animation.Play("TrapFallAnim");
+			Platform02.animation.Play("TrapFallAnim02");
+			Invoke("StopAnimPlatform01", Platform01.animation.clip.length + 0.345f);
+			Invoke ("StopAnimPlatform02", Platform02.animation.clip.length + 0.345f);
+			Indicate = 1;
+		}
+		Player.transform.position = pos;
+	}
+
+	void StopAnimPlatform01()
+	{
+		Platform01.animation.Stop ("TrapFallAnim");
+	}
+
+	void StopAnimPlatform02()
+	{
+		Platform02.animation.Stop ("TrapFallAnim02");
+	}
+}
---
+++ Assets/Scrips Perso/EventTrapWall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTrapWall : MonoBehaviour {
+	
+	public GameObject WallAnim;
+	public GameObject Player;
+	static float Indicate = 0;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		GameObject Wall = gameObject;
+		Vector2 pos = Player.transform.position; // if the player overtake the right border of the wall
+		if (pos.x > Wall.transform.position.x + Wall.transform.localScale.x + 1f && Indicate == 0)
+		{
+			WallAnim.animation.Play("TrapWallAnim"); // then we play the animation of the wall
+			Indicate = 1;
+		}
+		Player.transform.position = pos;
+	}
+}
