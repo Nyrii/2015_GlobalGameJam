48f849eacb9c33eb68e51336520b63b639e060c4
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 12:44:31 +0100

    toto

---
+++ Assets/ReadVideo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadVideo : MonoBehaviour {
+    public MovieTexture texture;
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log(texture);
+        texture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
