dc49cf21a734e6cc56a755f7820475f67206334c
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 13:14:08 +0100

    Menu quite ok

---
+++ Assets/Scripts Perso/ReadVideo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadVideo : MonoBehaviour {
+
+    public MovieTexture texture;
+
+    void Start () 
+    {
+        InvokeRepeating("LaunchVideo", 0, 5.9f);
+	}
+	
+	void LaunchVideo()
+    {
+        texture.Stop();
+        Debug.Log("TOTO");
+        texture.Play();
+    }
+}
