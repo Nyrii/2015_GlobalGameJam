df67f40e65382f38a527b6ce92193d6ec0ee1083
Fernand <fernand.veyrier@epitech.eu>
2015-01-24 08:54:52 +0100

    c'est reparti

--- Assets/Scrips Perso/Karma.cs
+++ Assets/Scrips Perso/Karma.cs
@@ -26,10 +26,7 @@ public class Karma : MonoBehaviour {
     void Update()
     {
         karmaAmount = playerStats.karmaAmount;
-        Debug.Log(karmaAmount);
-        Debug.Log("load = " + Mathf.Clamp01(1 - 2 * karmaAmount));
         karmaEvilGauge.fillAmount = Mathf.SmoothDamp(karmaEvilGauge.fillAmount, Mathf.Clamp01(1 - 2 * karmaAmount), ref DecreaseVel, 0.3f);
         karmaGoodGauge.fillAmount = Mathf.SmoothDamp(karmaGoodGauge.fillAmount, Mathf.Clamp01(2 * karmaAmount - 1f), ref DecreaseVel2, 0.3f);
-        //DecreaseVel = 0.1f;
     }
 }
