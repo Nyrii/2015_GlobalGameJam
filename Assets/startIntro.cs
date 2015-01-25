e0b7b89dcf24579436cd96e295608e449cf3fa31
Fernand <fernand.veyrier@epitech.eu>
2015-01-25 15:43:37 +0100

    video

---
+++ Assets/startIntro.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startIntro : MonoBehaviour {
+
+    public MovieTexture texture;
+
+    void Start()
+    {
+        LaunchVideo();
+        Invoke("LoadNext", 18f);
+    }
+
+    void LaunchVideo()
+    {
+        texture.Stop();
+        texture.Play();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNext();
+        }
+    }
+
+    void LoadNext()
+    {
+        Application.LoadLevel("level1Bis");
+    }
+}
