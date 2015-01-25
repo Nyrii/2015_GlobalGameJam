1a91c4e4ad7199b7c5880b764f8393d403ecc23a
Fernand <fernand.veyrier@epitech.eu>
2015-01-25 12:30:41 +0100

    added screams

--- Assets/Scripts Perso/BossScript.cs
+++ Assets/Scripts Perso/BossScript.cs
@@ -10,6 +10,7 @@ public class BossScript : MonoBehaviour {
     public GameObject spawnPoint;
     public GameObject canvasVictory;
     public GameObject[] canvas;
+    public AudioClip[] screams;
     public float shotDelay = 3f;
     float lastShot = 0;
     public float life = 1;
@@ -44,6 +45,14 @@ public class BossScript : MonoBehaviour {
         if (Random.Range(0, 10) == 9 && lastShot < 0)
         {
             lastShot = shotDelay;
+            if (player.GetComponent<PlatformerCharacter2D>().karmaAmount >= 0.5)
+            {
+                audio.PlayOneShot(screams[0]);
+            }
+            else
+            {
+                audio.PlayOneShot(screams[1]);
+            }
             GameObject go = (GameObject) Instantiate(bullet, spawnPoint.transform.position, Quaternion.identity);
             go.rigidbody2D.AddForce((player.transform.position - this.transform.position).normalized * 300F);
         }
@@ -54,9 +63,18 @@ public class BossScript : MonoBehaviour {
         healthBar.fillAmount = Mathf.SmoothDamp(healthBar.fillAmount, currentLife, ref currentVel, 0.5f);
         if (currentLife <= 0)
         {
+            if (player.GetComponent<PlatformerCharacter2D>().karmaAmount >= 0.5)
+            {
+                audio.PlayOneShot(screams[2]);
+            }
+            else
+            {
+                audio.PlayOneShot(screams[3]);
+            }
             canvasVictory.SetActive(true);
             healthBar.fillAmount = 0;
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, screams[3].length);
+            //this.gameObject.SetActive(false);
         }
 	}
 
